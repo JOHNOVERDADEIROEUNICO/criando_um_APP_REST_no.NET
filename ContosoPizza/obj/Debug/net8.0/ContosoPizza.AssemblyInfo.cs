d5199e42918c6e64acc9097f097978738ff833d8
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2026-08-12 09:45:53 -0300

    Model, Controller, Service

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4196fc470f12e438c0e5bb54ea9b583a82f3c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c6d8e969d508d538ed0b7f2582550b1385a5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
