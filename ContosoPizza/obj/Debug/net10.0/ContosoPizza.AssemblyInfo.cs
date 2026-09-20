9930275b70d875c2ba9fe3c058a98984e9882bd9
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2026-09-20 17:03:34 -0300

    finalizando um CRUD em 1 hora

--- ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481a1a15fdde957384899e931aeca5b2b3f1cee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069741e364955804db5b9106e3908017642e4be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
